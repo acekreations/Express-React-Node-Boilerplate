b29af33f3af2112c9d633f5f2cf210525b6a3804
Craig Melville <acekreations@gmail.com>
2018-10-08 19:39:31 -0700

    add Router in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <h1>Hello World</h1>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
