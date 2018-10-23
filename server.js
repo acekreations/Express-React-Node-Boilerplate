55c9d8aa32bfb8405579a7de60a7a84dffd6e765
Craig Melville <acekreations@gmail.com>
2018-10-23 14:54:44 -0700

    fixing deploy issue

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const routes = require("./routes");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,7 +12,7 @@ app.use(routes);
 
 app.use(express.static("../client/public"));
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, "client", "build")));
 }
 
 app.listen(PORT, function() {
